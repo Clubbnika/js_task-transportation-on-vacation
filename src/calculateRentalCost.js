e37efe49323b225d75cfda74db0fa8662f7d12bf
Veronika Okhten <veronika.oxt12@gmail.com>
2025-04-22 16:32:34 +0300

    fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,15 +5,19 @@
  */
 
 function calculateRentalCost(days) {
-  const result = days * 40;
+  if (typeof days !== 'number' || days <= 0 || !Number.isInteger(days)) {
+    return "Please enter a valid number of days.";
+  }
+
+  const dailyRate = 40;
+  let result = days * dailyRate;
 
   if (days >= 7) {
-    return result - 50;
+    result -= 50;
   } else if (days >= 3) {
-    return result - 20;
+    result -= 20;
   }
 
   return result;
 }
-
 module.exports = calculateRentalCost;
